f7f228dada4e8975642f620f4db7e0a1eb77f17d
Brooke Rhodes <podcast46231@gmail.com>
2020-11-28 22:12:55 +0000

    Add "this" to class methods

--- src/Icons.d.ts
+++ src/Icons.d.ts
@@ -128,24 +128,24 @@ declare interface Icon {
      * Expands the icon and sets a piece of text that appears next to
      * the image. Setting to `nil` or `""` will remove the label.
      */
-    setLabel: (text: string) => void
+    setLabel(this: Icon, text: string): void
 
     /**
      * Creates a box that appears below the icon when hovered
      * over. Setting to `nil` or `""` will remove the caption.
      */
-    setCaption: (text: string) => void
+    setCaption(this: Icon, text: string): void
 
     /**
      * Sets a tip that is displayed when hovering over the icon.
      * Setting to `nil` or `""` will remove the tip.
      */
-    setTip: (text: string) => void
+    setTip(this: Icon, text: string): void
 
     /**
      * Overrides the normal tip, if the player is in controller mode.
      */
-    setControllerTip: (text: string) => void
+    setControllerTip(this: Icon, text: string): void
 
     /**
      * Creates a dropdown that will be shown when the icon is right-clicked
@@ -153,104 +153,104 @@ declare interface Icon {
      * already is an `icon.dropdown`, `icon:removeDropdown()` will be called
      * before creating a new one.
      */
-    createDropdown: (options: IDropdownOption[]) => Dropdown
+    createDropdown(this: Icon, options: IDropdownOption[]): Dropdown
 
     /**
      * Destroys and removes all references of `icon.dropdown`.
      */
-    removeDropdown: () => void
+    removeDropdown(this: Icon): void
 
     /**
      * Sets the icons image, where `imageId` can be an int representing an asset
      * id (such as `4882428756`), or a string representing an assets pathway (such
      * as `"rbxasset://textures/ui/TopBar/chatOff.png"`).
      */
-    setImage: (image: string) => void
+    setImage(this: Icon, image: string): void
 
     /**
      * Sets the icons priority order, determining whether it will appear before
      * or after other icons.
      */
-    setOrder: (order: number) => void
+    setOrder(this: Icon, order: number): void
 
     /**
      * Aligns the icon on the left-side of the topbar (this happens by default).
      * The greater the `order`, the further rightward the icon will appear relative
      * to other icons set-left.
      */
-    setLeft: () => void
+    setLeft(this: Icon): void
 
     /**
      * Aligns the icon in the middle of the topbar. The greater the `order`, the
      * further rightward the icon will appear relative to other icons set-mid.
      */
-    setMid: () => void
+    setMid(this: Icon): void
 
     /**
      * Aligns the icon on the right-side of the topbar, next to the leaderboard/
      * emotes/inventory toggle. The greater the `order`, the further rightward the
      * icon will appear relative to other icons set-right.
      */
-    setRight: () => void
+    setRight(this: Icon): void
 
     /**
      * Sets the image size in pixels. Height will equal width if not specified.
      */
-    setImageSize: (width: number, height?: number) => void
+    setImageSize(this: Icon, width: number, height?: number): void
 
     /**
      * Sets the icons visibility.
      */
-    setEnabled: (enabled: boolean) => void
+    setEnabled(this: Icon, enabled: boolean): void
 
     /**
      * Changes the container size of icon to be `X pixels` by `Y pixels`. Defaults
      * to 32.
      */
-    setCellSize: (pixels: number) => void
+    setCellSize(this: Icon, pixels: number): void
 
     /**
      * Calculates the difference between the existing baseZIndex (i.e. the
      * `object.container.ZIndex`) and new value, then updates the ZIndex of all
      * objects within the icon accordingly using this difference.
      */
-    setBaseZIndex: (zindex: string) => void
+    setBaseZIndex(this: Icon, zindex: string): void
 
     /**
      * You must ensure the GuiObject has 'ResetOnSpawn' set to `false`, or that
      * you are calling `icon:setToggleMenu(guiObject)` every time the player
      * respawns, for the menu to persist.
      */
-    setToggleMenu: (guiObject: BasePlayerGui) => void
+    setToggleMenu(this: Icon, guiObject: BasePlayerGui): void
 
     /**
      * Sets a function that is called every time the icon is selected and
      * deselected.
      */
-    setToggleFunction: (toggleFunction: Callback) => void
+    setToggleFunction(this: Icon, toggleFunction: Callback): void
 
     /**
      * A function that is called when the icon is highlighted and
      * unhighlighted. The first argument passed is `icon.hovering`.
      */
-    setHoverFunction: (hoverFunction: (hovering?: boolean) => void) => void
+    setHoverFunction(this: Icon, hoverFunction: (hovering?: boolean) => void): void
 
     /**
      * Applies the specified theme to the icon.
      */
-    setTheme: (theme: IThemeStructure) => void
+    setTheme(this: Icon, theme: IThemeStructure): void
 
     /**
      * If set to true, disables the state overlay (the color and transparency
      * changes when an icon is hovered over and pressed).
      */
-    disableStateOverlay: (bool: boolean) => void
+    disableStateOverlay(this: Icon, bool: boolean): void
 
     /** Selects the icon. */
-    select: () => void
+    select(this: Icon): void
 
     /** Deselects the icon. */
-    deselect: () => void
+    deselect(this: Icon): void
 
     /**
      * Prompts a notification that appears in the top-right corner of the
@@ -258,16 +258,16 @@ declare interface Icon {
      * end the notification. If not specified, clearNoticeEvent defaults to
      * icon.deselected.
      */
-    notify: (clearNoticeEvent: RBXScriptSignal) => void
+    notify(this: Icon, clearNoticeEvent: RBXScriptSignal): void
 
     /** Clears all notifications. */
-    clearNotifications: () => void
+    clearNotifications(this: Icon): void
 
     /**
      * Destroys all instances, connections and signals associated with
      * the icon.
      */
-    destroy: () => void
+    destroy(this: Icon): void
 }
 
 declare interface IconConstructor {
